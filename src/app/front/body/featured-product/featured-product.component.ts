7d8ea8edb575e8ed18dd662f52b56885c1b7d59b
Mahmoud Hassan <hsn.mhmod.96@gmail.com>
2021-05-28 19:15:39 +0200

    add products images

--- src/app/front/body/featured-product/featured-product.component.ts
+++ src/app/front/body/featured-product/featured-product.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-featured-product',
   templateUrl: './featured-product.component.html',
-  styleUrls: ['./featured-product.component.scss']
+  styleUrls: ['./featured-product.component.scss'],
 })
 export class FeaturedProductComponent implements OnInit {
+  products: any = [
+    {
+      img: 'https://souqcms.s3-eu-west-1.amazonaws.com/cms/boxes/img/desktop/1622117970_banner-2.jpg',
+      title: 'product 1',
+      price: 22.99,
+    },
+    {
+      img: 'https://souqcms.s3-eu-west-1.amazonaws.com/cms/boxes/img/desktop/1622117970_banner-3.jpg',
+      title: 'product 1',
+      price: 22.99,
+    },
+    {
+      img: 'https://souqcms.s3-eu-west-1.amazonaws.com/cms/boxes/img/desktop/1622117971_banner-5.jpg',
+      title: 'product 3',
+      price: 10.99,
+    },
+    {
+      img: 'https://souqcms.s3-eu-west-1.amazonaws.com/cms/boxes/img/desktop/1622117971_banner-4.jpg',
+      title: 'product 4',
+      price: 11.001,
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/front/body/recent-product/recent-product.component.ts
+++ src/app/front/body/recent-product/recent-product.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-recent-product',
   templateUrl: './recent-product.component.html',
-  styleUrls: ['./recent-product.component.scss']
+  styleUrls: ['./recent-product.component.scss'],
 })
 export class RecentProductComponent implements OnInit {
+  products: any = [
+    {
+      img: 'https://souqcms.s3-eu-west-1.amazonaws.com/cms/boxes/img/desktop/1622117970_banner-2.jpg',
+      title: 'product 1',
+      price: 22.99,
+    },
+    {
+      img: 'https://souqcms.s3-eu-west-1.amazonaws.com/cms/boxes/img/desktop/1622117970_banner-3.jpg',
+      title: 'product 1',
+      price: 22.99,
+    },
+    {
+      img: 'https://souqcms.s3-eu-west-1.amazonaws.com/cms/boxes/img/desktop/1622117971_banner-5.jpg',
+      title: 'product 3',
+      price: 10.99,
+    },
+    {
+      img: 'https://souqcms.s3-eu-west-1.amazonaws.com/cms/boxes/img/desktop/1622117971_banner-4.jpg',
+      title: 'product 4',
+      price: 11.001,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
